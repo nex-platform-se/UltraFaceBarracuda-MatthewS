c97f14312c2a11bfe57765d6c5604a6a1e036ab4
kahonex <kaho@nex.team>
2025-02-27 19:12:56 +0800

    question preview

--- Assets/Visualizer.cs
+++ Assets/Visualizer.cs
@@ -1,68 +1,113 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UI = UnityEngine.UI;
 using Klak.TestTools;
+using UnityEngine.Profiling;
 
-namespace UltraFace {
-
-public sealed class Visualizer : MonoBehaviour
+namespace UltraFace
 {
-    #region Editable attributes
-
-    [SerializeField] ImageSource _source = null;
-    [SerializeField, Range(0, 1)] float _threshold = 0.5f;
-    [SerializeField] ResourceSet _resources = null;
-    [SerializeField] Shader _visualizer = null;
-    [SerializeField] Texture2D _texture = null;
-    [SerializeField] UI.RawImage _previewUI = null;
-
-    #endregion
-
-    #region Private objects
-
-    FaceDetector _detector;
-    Material _material;
-    ComputeBuffer _drawArgs;
-
-    #endregion
-
-    #region MonoBehaviour implementation
-
-    void Start()
+    public sealed class Visualizer : MonoBehaviour
     {
-        _detector = new FaceDetector(_resources);
-        _material = new Material(_visualizer);
-        _drawArgs = new ComputeBuffer(4, sizeof(uint),
-                                      ComputeBufferType.IndirectArguments);
-        _drawArgs.SetData(new [] {6, 0, 0, 0});
+        [SerializeField] ImageSource _source = null;
+        [SerializeField, Range(0, 1)] float _threshold = 0.5f;
+        [SerializeField] ResourceSet _resources = null;
+        [SerializeField] UI.RawImage _previewUI = null;
+        [SerializeField] private UI.Text memoryText;
+
+        FaceDetector _detector;
+
+        private float previewWidth;
+        private float previewHeight;
+        private List<UI.RawImage> boxes = new();
+
+        void Start()
+        {
+            _detector = new FaceDetector(_resources);
+            RectTransform previewRect = _previewUI.rectTransform;
+            previewWidth = previewRect.rect.width; //Preview height is fixed
+            previewHeight = previewRect.rect.width; //Preview height is fixed
+        }
+
+        void Update()
+        {
+            _detector.ProcessImage(_source.Texture, _threshold);
+            
+            _previewUI.texture = _source.Texture;
+
+            DestroyAllBoxes();
+            VisualizeResults();
+            ShowMemoryUsage();
+        }
+
+        #region Core functions
+        void VisualizeResults()
+        {
+            foreach (var detection in _detector.Detections)
+            {
+                var box = CreateRedBox(_previewUI.transform); // Create new red box
+                boxes.Add(box);
+                RenderBox(detection, box); // Update the red box position and size based on the detection result.
+            }
+        }
+
+        UI.RawImage CreateRedBox(Transform parent)
+        {
+            GameObject redBox = new GameObject("RedBox", typeof(RectTransform), typeof(UI.RawImage));
+            redBox.transform.SetParent(parent, false);
+            // Get RectTransform and adjust settings
+            RectTransform rect = redBox.GetComponent<RectTransform>();
+            rect.anchorMin = new Vector2(0, 1);
+            rect.anchorMax = new Vector2(0, 1);
+            rect.pivot = new Vector2(0, 1);
+
+            UI.RawImage image = redBox.GetComponent<UI.RawImage>();
+            image.color = Color.red; // Set red color
+
+            return image;
+        }
+
+        void RenderBox(Detection detection, UI.RawImage box)
+        {
+            if (_previewUI == null) return;
+            
+            box.gameObject.SetActive(true); //Display the box
+            float x1 = detection.x1;
+            float y1 = detection.y1;
+            float x2 = detection.x2;
+            float y2 = detection.y2;
+
+            float posX = x1 * previewWidth;
+            float posY = -y1 * previewHeight;
+            float w = (x2 - x1) * previewWidth;
+            float h = (y2 - y1) * previewHeight;
+
+
+            // Set the new box position
+            RectTransform boxRect = box.rectTransform;
+            boxRect.anchoredPosition = new Vector2(posX, posY);
+            boxRect.sizeDelta = new Vector2(w, h);
+        }
+
+        #endregion
+
+        void DestroyAllBoxes()
+        {
+            foreach (var box in boxes)
+            {
+                Destroy(box.gameObject);
+            }
+
+            boxes.Clear();
+        }
+
+        void ShowMemoryUsage()
+        {
+            memoryText.text = $"Total Allocated Memory: {Profiler.GetTotalAllocatedMemoryLong() / (1024 * 1024)} MB";
+        }
+        
+        void OnDestroy()
+        {
+            _detector?.Dispose();
+        }
     }
-
-    void OnDestroy()
-    {
-        _detector?.Dispose();
-        Destroy(_material);
-        _drawArgs?.Dispose();
-    }
-
-    void Update()
-    {
-        _detector.ProcessImage(_source.Texture, _threshold);
-        _previewUI.texture = _source.Texture;
-
-        if (Input.GetMouseButtonDown(0))
-            foreach (var d in _detector.Detections) Debug.Log(d);
-    }
-
-    void OnRenderObject()
-    {
-        _detector.SetIndirectDrawCount(_drawArgs);
-        _material.SetFloat("_Threshold", _threshold);
-        _material.SetTexture("_Texture", _texture);
-        _material.SetBuffer("_Detections", _detector.DetectionBuffer);
-        _material.SetPass(_texture == null ? 0 : 1);
-        Graphics.DrawProceduralIndirectNow(MeshTopology.Triangles, _drawArgs, 0);
-    }
-
-    #endregion
-}
-
 } // namespace UltraFace
